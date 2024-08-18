a3e2be8d461b7974e3bde1c540be08ad31b2e501
Daniel Remoquillo <remoquillodr@gmail.com>
2024-08-18 20:44:34 +0800

    🚀 Deploy Vite project to GitHub Pages 🌐 | Updated vite.config.js for correct base path 📁

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://danielremoquillo.github.io/react-try-and-learn-todo/", // replace with your repo name
+  build: {
+    outDir: "dist", // default output directory
+  },
   plugins: [react(), svgr()],
   server: {
     port: 3000,
